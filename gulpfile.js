3eb062eaaf155fad869584ba479381bd2c5caeb3
Jelmer Tjoelker <110289451+tjoelker@users.noreply.github.com>
2022-07-30 23:41:40 +0200

    Added babel to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+
+// compile js
+function script() {
+  return gulp.src('./js/**/*.js')
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(gulp.dest('./src'));
+};
 
 // compile scss -> css
 function style() {
@@ -8,7 +18,7 @@ function style() {
     .pipe(sass().on('error', sass.logError)) // 2: pass the file(s) through the sass compiler
     .pipe(gulp.dest('./css'))                // 3: location where the css file should go
     .pipe(browserSync.stream());             // 4: stream changes to browser(s)
-}
+};
 
 // synchronize new changes with browser output
 function sync() {
@@ -17,10 +27,11 @@ function sync() {
       baseDir: './'
     }
   });
-  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js', script).on('change', browserSync.reload);
   gulp.watch('./scss/**/*.scss', style);
   gulp.watch('./*.html').on('change', browserSync.reload);
-}
+};
 
+exports.script = script;
 exports.style = style;
 exports.sync = sync;
