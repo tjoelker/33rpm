410e0ced9d2bb660d2fd4b92b2e2b6421d597c32
Jelmer Tjoelker <110289451+tjoelker@users.noreply.github.com>
2022-07-31 21:12:41 +0200

    Disk plays/pauses on click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('Success');
+const vinyl = document.querySelector('.disk');
+let playing = false;
+
+vinyl.onclick = () => {
+  if (playing == false) {
+    playing = true;
+    rotation.play();
+    console.log('play');
+  } else if (playing == true) {
+    playing = false;
+    rotation.pause();
+    console.log('pause');
+  }
+};
+
+const rotation = anime({
+  targets: '.disk',
+  autoplay: false,
+  loop: true,
+  rotate: {
+    easing: 'linear',
+    duration: 1800,
+    value: 360,
+  },
+});
--- src/script.js
+++ src/script.js
@@ -1,3 +1,27 @@
 "use strict";
 
-console.log('Success');
+var vinyl = document.querySelector('.disk');
+var playing = false;
+
+vinyl.onclick = function () {
+  if (playing == false) {
+    playing = true;
+    rotation.play();
+    console.log('play');
+  } else if (playing == true) {
+    playing = false;
+    rotation.pause();
+    console.log('pause');
+  }
+};
+
+var rotation = anime({
+  targets: '.disk',
+  autoplay: false,
+  loop: true,
+  rotate: {
+    easing: 'linear',
+    duration: 1800,
+    value: 360
+  }
+});
